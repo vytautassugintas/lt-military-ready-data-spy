6a4d27aa7d407d942b467ba8ffde24f509304831
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2018-01-18 21:52:23 +0200

    feat: running data collection every minute

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 const osmosis = require("osmosis");
+const schedule = require("node-schedule");
+
+const url =
+  "https://kam.lt/lt/aktuali_informacija_apie_privalomaja_karine_tarnyba/new_2716.html";
 
 let result = [];
 
-osmosis
-  .get(
-    "https://kam.lt/lt/aktuali_informacija_apie_privalomaja_karine_tarnyba/new_2716.html"
-  )
-  .find("table tr td strong")
-  .set("data")
-  .data(function(data) {
-    result = [...result, data];
-  });
+console.log("Starting data collection from " + url);
+const job = schedule
+  .scheduleJob("*/1 * * * *", () => {
+    osmosis
+      .get(url)
+      .find("table tr td strong")
+      .set("data")
+      .data(data => {
+        result = [...result, data.data];
+      });
 
-setTimeout(() => {
-  const data = {
-    statedWishForDuty: result[15].data,
-    statedWishForDutyProcedures: result[16].data,
-    pledgedPriorityForDuty: result[17].data,
-    pledgedPriorityForDutyProcedures: result[18].data,
-    compulsoryAppropriateForDuty: result[19].data,
-    preliminaryTotal:
-      parseInt(result[16].data) +
-      parseInt(result[18].data) +
-      parseInt(result[19].data)
-  };
-  console.log(data);
-}, 5000);
+    setTimeout(() => {
+      const data = {
+        statedWishForDuty: result[15],
+        statedWishForDutyProcedures: result[16],
+        pledgedPriorityForDuty: result[17],
+        pledgedPriorityForDutyProcedures: result[18],
+        compulsoryAppropriateForDuty: result[19],
+        preliminaryTotal:
+          parseInt(result[16]) +
+          parseInt(result[18]) +
+          parseInt(result[19])
+      };
+      console.log(data);
+    }, 10000);
+  }); 
